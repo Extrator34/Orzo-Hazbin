061f233d281f69cd85d85d74aa22d6ede9a74f52
Extrator34 <116797725+Extrator34@users.noreply.github.com>
2025-11-08 17:09:29 +0100

    Create raceAbilities.js

---
+++ raceAbilities.js
@@ -0,0 +1,32 @@
+// raceAbilities.js
+export const raceAbilities = {
+  hellhound: [
+    { nome: "Olfatto sopraffino", descrizione: "Sentire odori in modo superiore", livello: 1 },
+    { nome: "Udito sopraffino", descrizione: "Percepire rumori impercettibili", livello: 1 },
+    { nome: "Vista eccezionale", descrizione: "Osservare dettagli con precisione", livello: 1 }
+  ],
+  succube: [
+    { nome: "Tempra Afrodisiaca", descrizione: "Resistenza e vigore in ambito intimo", livello: 1 },
+    { nome: "Cantare", descrizione: "Incantare con la voce", livello: 1 },
+    { nome: "Suonare", descrizione: "Suonare strumenti musicali", livello: 1 } // nuova abilità
+  ],
+  imp: [
+    { nome: "Furtività Urbana", descrizione: "Muoversi senza essere visti", livello: 1 },
+    { nome: "Parkour Infernale", descrizione: "Acrobazie tra strutture decadenti", livello: 1 },
+    // scelta tra tre abilità: gestita a parte
+  ],
+  bafometto: [
+    { nome: "Medicina Non Convenzionale", descrizione: "Curare con metodi alternativi", livello: 1 },
+    { nome: "Resistenza ad Alcol/Droghe", descrizione: "Sopportare e sfruttare sostanze", livello: 1 },
+    { nome: "Nuoto Estremo", descrizione: "Sopravvivere in liquidi corrosivi", livello: 1 }
+  ],
+  infestatore: [
+    { nome: "Inganni e Trappole", descrizione: "Creare diversivi e trappole visive", livello: 1 },
+    { nome: "Gestione Illecita", descrizione: "Gestire scambi e micro-crimini", livello: 1 },
+    { nome: "Burocrazia Infernale", descrizione: "Manipolare documenti di dannazione", livello: 1 }
+  ],
+  cherubino: [
+    { nome: "Volare", descrizione: "Saper usare le ali", livello: 1 },
+    { nome: "Corpo a Corpo Sacro", descrizione: "Difesa fisica con tecniche armoniose", livello: 1 }
+  ]
+};
