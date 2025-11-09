ad2e372ca2e09211b7aacf6b3178301d26911b52
Extrator34 <116797725+Extrator34@users.noreply.github.com>
2025-11-09 19:12:12 +0100

    Update askRace.js

--- askRace.js
+++ askRace.js
@@ -29,30 +29,3 @@ export async function askRace({ interaction, characterName }) {
   });
 }
 
-
-export async function askRace({ interaction, characterName }) {
-  const raceOptions = [
-    "Peccatore", "Hellhound", "Succube", "Imp",
-    "Bafometto", "Infestatore", "Winner",
-    "Cherubino", "Angelo caduto"
-  ];
-
-  const menu = new StringSelectMenuBuilder()
-    // encodeURIComponent per rendere sicuro il nome
-    .setCustomId(`select_race_${interaction.user.id}_${encodeURIComponent(characterName)}`)
-    .setPlaceholder("Scegli la razza del tuo personaggio")
-    .addOptions(
-      raceOptions.map(r => ({
-        label: r,
-        value: r.toLowerCase().replace(/ /g, "_")
-      }))
-    );
-
-  const row = new ActionRowBuilder().addComponents(menu);
-
-  await interaction.channel.send({
-    content: `🧬 <@${interaction.user.id}>, scegli la razza per **${characterName}**:`,
-    components: [row],
-    flags: MessageFlags.Ephemeral
-  });
-}
