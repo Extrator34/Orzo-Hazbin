23cc497c63aa14d4f0114f33b2a050fda02916ff
Extrator34 <116797725+Extrator34@users.noreply.github.com>
2025-11-09 18:24:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -758,6 +758,7 @@ for (let i = 0; i < celestiali.length; i += 25) {
 await interaction.reply({
   content: `📜 Seleziona un'abilità da aggiungere o incrementare per **${char.name}**:`,
   components: rows,
+  flags: MessageFlags.Ephemeral
 });
 
 }
@@ -799,6 +800,7 @@ if (existing) {
     // opzionale: messaggio extra
     await interaction.followUp({
       content: `📜 Log: abilità incrementata`,
+      flags: MessageFlags.Ephemeral
     });
 
   } else {
@@ -823,6 +825,7 @@ if (existing) {
   // opzionale: messaggio extra
   await interaction.followUp({
     content: `📜 Log: nuova abilità aggiunta`,
+    flags: MessageFlags.Ephemeral
   });
 }
 }
